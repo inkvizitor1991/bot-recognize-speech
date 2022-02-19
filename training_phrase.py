0f01cd2f422b749f1c56cd25bbdfd2074ca86bae
inkvizitor1991 <85788805+inkvizitor1991@users.noreply.github.com>
2022-02-19 16:06:59 +0300

    Deleted the extra code

--- training_phrase.py
+++ training_phrase.py
@@ -9,38 +9,27 @@ from google.cloud import dialogflow
 logger = logging.getLogger(__name__)
 
 
-def create_intent(training_phrase):
-    with open(training_phrase, 'r') as my_file:
-        intents = json.load(my_file)
-        for intent in intents:
-            training_phrases_parts = intents[intent]['questions']
-            message_answer = [intents[intent]['answer']]
-            project_id = os.environ['DIALOG_FLOW_PROJECT_ID']
-            intents_client = dialogflow.IntentsClient()
-            parent = dialogflow.AgentsClient.agent_path(project_id)
-            training_phrases = []
-            for training_phrases_part in training_phrases_parts:
-                part = dialogflow.Intent.TrainingPhrase.Part(
-                    text=training_phrases_part
-                )
-                training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
-                training_phrases.append(training_phrase)
-            text = dialogflow.Intent.Message.Text(text=message_answer)
-            message = dialogflow.Intent.Message(text=text)
-            intent = dialogflow.Intent(
-                display_name=intent, training_phrases=training_phrases,
-                messages=[message]
-            )
-
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    intents_client = dialogflow.IntentsClient()
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(
+            text=training_phrases_part
+        )
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases,
+        messages=[message]
+    )
     intents_client.create_intent(
         request={"parent": parent, "intent": intent}
     )
 
 
-def open_training_phrase(training_phrase):
-    with open(training_phrase, 'r') as my_file:
-        intents = json.load(my_file)
-        return intents
 
 
 def get_parser():
@@ -61,4 +50,12 @@ if __name__ == '__main__':
     parser = get_parser()
     args = parser.parse_args()
     training_phrase = args.json_path
-    create_intent(training_phrase)
+
+    with open(training_phrase, 'r') as my_file:
+        intents = json.load(my_file)
+        for intent in intents:
+            training_phrases = intents[intent]['questions']
+            message_answer = [intents[intent]['answer']]
+            project_id = os.environ['DIALOG_FLOW_PROJECT_ID']
+            create_intent(project_id, intent, training_phrases, message_answer)
+
